e903f1fb2782a2d921769923f60289cafc773947
ALI ALRAWI <ali.developer90@gmail.com>
2016-10-15 08:51:57 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
 
+class CashRegister
+
+#attr_accessor :cash_register , :cash_register_with_discount
+attr_accessor :total , :discount
+attr_reader :items
+def initialize(discount=20)
+  @total=0
+  @discount=discount
+  @items={}
+
+end
+def add_item(title,price,quantity=1)
+   items[title]=price
+   total =(total)+ (price*quantity)
+end
+
+
+end
